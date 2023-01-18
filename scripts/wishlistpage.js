810ded0a8c7957027fa8676c1a08b69c1f98410c
Your Name <you@example.com>
2023-01-18 21:06:53 +0530

    wishlist css

---
+++ scripts/wishlistpage.js
@@ -0,0 +1,10 @@
+// let container = document.getElementById("card-container");
+
+// let cartData = JSON.parse(localStorage.getItem("cart"))||[];
+
+// function displayProducts(data) {
+    
+//     container.innerHTML = null;
+
+//     let 
+// }
