4ac1b7d5244acc35e30ff8235b6596008238361d
DishaGup <guptamansi201@gmail.com>
2023-01-20 10:40:05 +0530

    day-4

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,47 @@
 
 let furnitureContainer=document.getElementById("furniture-container")
  let furnitureContainermouseover=document.getElementById("furniture-container-mouseover")
-
+ let kitchenContainer=document.getElementById("Kitchen-container")
+ let kitchenContainermouseover=document.getElementById("Kitchen-container-mouseover")
  let delay5000=function () {
     setTimeout(() => {
-        
+        Location.reload()
     }, 5000);
  }
 
+
+
+
+ kitchenContainermouseover.addEventListener("mouseover",()=>{
+    kitchenContainer.style.display="flex"
+  setTimeout(() => {
+    kitchenContainer.style.display="none"
+  }, 5000);
+
+})
+kitchenContainermouseover.addEventListener("mouseleave",()=>{
+    kitchenContainer.style.display="none"
+})
+
+
 furnitureContainermouseover.addEventListener("mouseover",(e)=>{
     furnitureContainer.style.display="flex"
+    setTimeout(() => {
+        furnitureContainer.style.display="none"
+      }, 5000);
 
-setTimeout(() => {
-    furnitureContainer.style.display="none"
-},5000);
 })
+furnitureContainermouseover.addEventListener("mouseout",()=>{
+    furnitureContainer.style.display="none"
+})
+
 let popdiv=document.getElementById("pop-div")
+popdiv.addEventListener("click",removepopup)
 function removepopup(){
 popdiv.style.bottom="-50%"
 
 }
+
 
 
 
